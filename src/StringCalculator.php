381a655ede598bb7ce235ef806d7956edbdbd0e8
Bernardeiro <iribarren.148642@e.unavarra.es>
2025-03-29 11:56:26 +0100

    [#] - Refactorizado StringCalculator encapsulando métodos en funciones auxiliares

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -18,13 +18,7 @@ class StringCalculator
         if (str_contains($numbers, '//')) {
             $numbers = $this->extraeSeparador($numbers);
 
-            foreach (explode(',', $numbers) as $number) {
-                if ($number < 0) {
-                    $negativeNumbers[] = $number;
-                } else if ($number > 1000) {
-                    $numbersHigherThan1000 .= $number;
-                }
-            }
+            list($negativeNumbers, $numbersHigherThan1000) = $this->analizaValoresMayoresDe1000yNegativos($numbers, $negativeNumbers, $numbersHigherThan1000);
             if (!empty($negativeNumbers)) {
                 throw new \Exception('Negativos no soportados: ' . implode(', ', $negativeNumbers));
             }
@@ -32,13 +26,7 @@ class StringCalculator
             return array_sum(explode(',', $numbers)) - $numbersHigherThan1000;
         }
 
-        foreach (explode(',', $numbers) as $number) {
-            if ($number < 0) {
-                $negativeNumbers[] = $number;
-            }else if ($number > 1000) {
-                $numbersHigherThan1000 .= $number;
-            }
-        }
+        list($negativeNumbers, $numbersHigherThan1000) = $this->analizaValoresMayoresDe1000yNegativos($numbers, $negativeNumbers, $numbersHigherThan1000);
         if (!empty($negativeNumbers)) {
             throw new \Exception('Negativos no soportados: ' . implode(', ', $negativeNumbers));
         }
@@ -59,5 +47,45 @@ class StringCalculator
         $delimiter = substr($numbers, 2, 1);
         $numbers = str_replace(['//' . $delimiter . ',', $delimiter], ['', ','], $numbers);
         return $numbers;
+    }
+
+    /**
+     * @param string $number
+     * @param array $negativeNumbers
+     * @return array
+     */
+    public function annadeNumeroNegativoAlListado(string $number, array $negativeNumbers): array
+    {
+        $negativeNumbers[] = $number;
+        return $negativeNumbers;
+    }
+
+    /**
+     * @param string $number
+     * @param string $numbersHigherThan1000
+     * @return string
+     */
+    public function sumarValoresSuperioresA1000(string $number, string $numbersHigherThan1000): string
+    {
+        $numbersHigherThan1000 .= $number;
+        return $numbersHigherThan1000;
+    }
+
+    /**
+     * @param array|string $numbers
+     * @param mixed $negativeNumbers
+     * @param mixed $numbersHigherThan1000
+     * @return array
+     */
+    public function analizaValoresMayoresDe1000yNegativos(array|string $numbers, mixed $negativeNumbers, mixed $numbersHigherThan1000): array
+    {
+        foreach (explode(',', $numbers) as $number) {
+            if ($number < 0) {
+                $negativeNumbers = $this->annadeNumeroNegativoAlListado($number, $negativeNumbers);
+            } else if ($number > 1000) {
+                $numbersHigherThan1000 = $this->sumarValoresSuperioresA1000($number, $numbersHigherThan1000);
+            }
+        }
+        return array($negativeNumbers, $numbersHigherThan1000);
     }
 }
